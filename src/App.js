e4aeb7dcb1b4a8fdd95232ac6f256d2ffe9188e6
smrutid12 <smrutid12@gmail.com>
2023-03-04 10:05:09 +0530

    Homepage basic design

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import Homepage from './pages/Homepage'
-import Events from './pages/Events'
-import Faq from './pages/Faq'
-import Gallery from './pages/Gallery'
-import Team from './pages/Team'
+// import Events from './pages/Events'
+// import Faq from './pages/Faq'
+// import Gallery from './pages/Gallery'
+// import Team from './pages/Team'
 import './static/css/App.css'
 
 
 export default function App() {
   return (
-    <>
-      <Homepage />
-      <Events />
-      <Faq />
-      <Gallery />
-      <Team />
-    </>
+    <div className='app'>
+      <div className='app_body'>
+        <div className='body_components'>
+          <Homepage />
+          {/* <Events />
+          <Faq />
+          <Gallery />
+          <Team /> */}
+        </div>
+      </div>
+    </div>
   )
 }
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
-
+import React from "react";
+import "../static/css/homepage.css";
+import logo from "../static/svg/zygonheaderlogo.svg";
+import siliconlogo from "../static/images/SiliconTech.png";
+import zygonlogo from "../static/svg/zygonlogo.svg";
 export default function Homepage() {
-	return (<>
-		<h1>Homepage</h1>
-	</>)
+  return (
+    <div className="homepage">
+      <div className="homepage_header">
+        <div className="row">
+          <div className="col col-1">
+            <img className="logo" src={logo} alt="logo" />
+          </div>
+          <div className="col col-2">
+            <img className="logo" src={siliconlogo} alt="logo" />
+          </div>
+        </div>
+      </div>
+      <div className="homepage_body">
+        <div>
+          <img className="hero_image" src={zygonlogo} alt="zygonlogo" />
+        </div>
+        <div className="body_header">
+          <h4>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s,
+          </h4>
+        </div>
+        <div className="body_links">
+          <div className="links">
+            <h3>Section 1</h3>
+          </div>
+          <div className="links">
+            <h3>Section 2</h3>
+          </div>
+          <div className="links">
+            <h3>Section 3</h3>
+          </div>
+          <div className="links">
+            <h3>Section 4</h3>
+          </div>
+          <div className="links">
+            <h3>Section 5</h3>
+          </div>
+        </div>
+      </div>
+      <div className="homepage_footer">
+        <h4>Developed by SiliconTech</h4>
+      </div>
+    </div>
+  );
 }
